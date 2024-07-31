134808e471e3e871200e0831ebd7224f5b745b51
huangb <huangbing@ecdigit.com>
2024-07-31 14:07:05 +0800

    请求，相应拦截

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -1,14 +1,22 @@
 import axios from 'axios';
 import type { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { Message, Modal } from '@arco-design/web-vue';
-import { useUserStore } from '@/store';
+import { Message } from '@arco-design/web-vue';
 import { getToken } from '@/utils/auth';
 
+// 添加自定义的一些请求配置
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+  needToken?: boolean;
+}
+
 export interface HttpResponse<T = unknown> {
-  status: number;
-  msg: string;
-  code: number;
-  data: T;
+  code?: string;
+  data?: T;
+  message?: string;
+  page?: number;
+  size?: number;
+  total?: number;
+  totalPageNum?: number;
+  success?: boolean;
 }
 
 if (import.meta.env.VITE_API_BASE_URL) {
@@ -16,17 +24,14 @@ if (import.meta.env.VITE_API_BASE_URL) {
 }
 
 axios.interceptors.request.use(
-  (config: AxiosRequestConfig) => {
-    // let each request carry token
-    // this example using the JWT token
-    // Authorization is a custom headers key
-    // please modify it according to the actual situation
+  (config: CustomAxiosRequestConfig) => {
+    const { needToken = true } = config;
     const token = getToken();
-    if (token) {
-      if (!config.headers) {
-        config.headers = {};
-      }
-      config.headers.Authorization = `Bearer ${token}`;
+    if (!config.headers) {
+      config.headers = {};
+    }
+    if (token && needToken) {
+      config.headers.Authorization = token;
     }
     return config;
   },
@@ -39,37 +44,30 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
   (response: AxiosResponse<HttpResponse>) => {
     const res = response.data;
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message.error({
-        content: res.msg || 'Error',
-        duration: 5 * 1000,
-      });
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (
-        [50008, 50012, 50014].includes(res.code) &&
-        response.config.url !== '/api/user/info'
-      ) {
-        Modal.error({
-          title: 'Confirm logout',
-          content:
-            'You have been logged out, you can cancel to stay on this page, or log in again',
-          okText: 'Re-Login',
-          async onOk() {
-            const userStore = useUserStore();
+    // if the custom code is not 200, it is judged as an error.
+    if (res.code === '200') return res;
 
-            await userStore.logout();
-            window.location.reload();
-          },
-        });
-      }
-      return Promise.reject(new Error(res.msg || 'Error'));
+    if (response.config.responseType === 'blob') {
+      return response;
     }
-    return res;
+
+    Message.error({
+      content: res.message || '未知错误',
+      duration: 5 * 1000,
+    });
+    return Promise.reject(res);
   },
   (error) => {
+    const errRes = error.response.data;
+    if (errRes.code === '401' || error.response.status === 401) {
+      // TODO 退出登录
+      return Promise.reject(
+        new Error('无效的会话，或者会话已过期，请重新登录。')
+      );
+    }
+
     Message.error({
-      content: error.msg || 'Request Error',
+      content: errRes.msg || '系统错误',
       duration: 5 * 1000,
     });
     return Promise.reject(error);
