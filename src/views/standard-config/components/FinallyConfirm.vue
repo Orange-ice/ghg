22e05fba59b7e9f2b2016ef1ee0ff8d40a3822e9
huangb <huangbing@ecdigit.com>
2024-08-07 14:38:11 +0800

    标准选择 周期选择

---
+++ src/views/standard-config/components/FinallyConfirm.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+  /**
+   * @description Component 标准创建确认
+   * */
+  const modelValue = defineModel<boolean>();
+</script>
+
+<template>
+  <a-modal
+    v-model:visible="modelValue"
+    title="标准创建确认"
+    title-align="start"
+    width="704px"
+    :cancel-button-props="{ type: 'outline' }"
+  >
+    <div class="container">
+      <div class="tip">
+        <iconpark-icon name="attention" class="icon" />
+        <span>
+          当前仅剩余
+          <strong>1</strong>
+          次标准创建机会，确认后无法修改，请再次核对信息准确无误。
+        </span>
+      </div>
+
+      <span class="title-text">核算标准</span>
+      <div class="standard">
+        <span>中国碳核算标准或指南</span>
+        <span>地方碳核算指南</span>
+      </div>
+      <div class="cycle">
+        <span class="title-text">所选行业/地区</span>
+        <span class="title-text">填报周期</span>
+        <div> 采矿业（上海）</div>
+        <div>季度填报</div>
+      </div>
+    </div>
+  </a-modal>
+</template>
+
+<style lang="less" scoped>
+  .container {
+    .title-text {
+      font-size: 12px;
+      color: #4e5969;
+      line-height: 24px;
+    }
+
+    .tip {
+      padding: 6px 16px;
+      background: #fff5eb;
+      margin-bottom: 20px;
+      display: flex;
+      align-items: center;
+
+      > .icon {
+        color: #ff7d00;
+        margin-right: 6px;
+        font-size: 12px;
+      }
+
+      > span {
+        font-size: 12px;
+        color: var(--text-1);
+        line-height: 22px;
+
+        > strong {
+          color: #ff7d00;
+        }
+      }
+    }
+
+    .standard {
+      margin: 12px 0 20px;
+      display: flex;
+      align-items: center;
+
+      > span:first-child {
+        font-size: 16px;
+        color: var(--text-1);
+        line-height: 24px;
+        font-weight: 600;
+        margin-right: 8px;
+      }
+
+      > span:last-child {
+        background: #e8fff1;
+        padding: 0 6px;
+        font-size: 14px;
+        color: #008f50;
+        line-height: 20px;
+        display: inline-block;
+      }
+    }
+
+    .cycle {
+      margin-bottom: 22px;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-gap: 12px 16px;
+
+      > div {
+        padding: 12px 16px;
+        background: #f7f8fa;
+        border-radius: 4px;
+
+        font-weight: 600;
+        font-size: 14px;
+        color: var(--text-1);
+        line-height: 24px;
+      }
+    }
+  }
+</style>
--- src/views/standard-config/cycle.vue
+++ src/views/standard-config/cycle.vue
@@ -1,8 +1,148 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import BaseLayout from '@/views/standard-config/components/BaseLayout.vue';
+  import FinallyConfirm from '@/views/standard-config/components/FinallyConfirm.vue';
+  import { ref } from 'vue';
+
+  const confirmVisible = ref(false);
+</script>
 
 <template>
-  <div>
-    填报周期
-    <a-button @click="$router.back()">返回</a-button>
-  </div>
+  <BaseLayout title="请选择填报周期" back-route="standardIndustry">
+    <span class="title-text">所选行业</span>
+    <div class="industry">
+      <span>采矿业（上海）</span>
+      <span>重新选择</span>
+    </div>
+
+    <div class="cycle">
+      <span class="title-text">填报周期</span>
+      <div class="cycle-wrapper">
+        <div class="cycle-item">
+          <span>年度填报</span>
+          <p>以年度为单位进行碳数据填报</p>
+        </div>
+        <div class="cycle-item">
+          <span>年度填报</span>
+          <p>以年度为单位进行碳数据填报</p>
+        </div>
+        <div class="cycle-item">
+          <span>年度填报</span>
+          <p>以年度为单位进行碳数据填报</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="tip">
+      <iconpark-icon name="attention" class="icon" />
+      <span>
+        您当前选择的是「XX填报」，请再次确认是否选用此填报周期，一旦选择则无法修改。
+      </span>
+    </div>
+
+    <div class="operation">
+      <a-button style="margin-right: 12px" type="outline">返回重选</a-button>
+      <a-button type="primary" @click="confirmVisible = true">
+        开始使用
+      </a-button>
+    </div>
+  </BaseLayout>
+
+  <FinallyConfirm v-model="confirmVisible" />
 </template>
+
+<style lang="less" scoped>
+  .title-text {
+    font-size: 14px;
+    color: var(--text-1);
+    line-height: 24px;
+    font-weight: bold;
+  }
+
+  .industry {
+    margin: 16px 0 32px;
+    padding: 16px;
+    background: #f7f8fa;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    > span:first-child {
+      font-weight: 600;
+      font-size: 16px;
+      color: var(--text-1);
+      line-height: 24px;
+    }
+
+    > span:last-child {
+      font-size: 14px;
+      color: #008f50;
+      line-height: 24px;
+      cursor: pointer;
+    }
+  }
+
+  .cycle {
+    padding-top: 32px;
+    border-top: solid 1px #f2f3f5;
+
+    .cycle-wrapper {
+      margin: 16px 0 24px;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-column-gap: 18px;
+
+      .cycle-item {
+        cursor: pointer;
+        border: solid 1px #a3a3a3;
+        transition: 0.3s;
+        border-radius: 4px;
+        padding: 16px 14px;
+
+        > span:first-child {
+          font-size: 16px;
+          color: var(--text-1);
+          line-height: 24px;
+          font-weight: 600;
+        }
+
+        > p {
+          margin-top: 4px;
+          font-size: 14px;
+          color: #86909c;
+          line-height: 24px;
+        }
+
+        &:hover {
+          border-color: #008f50;
+        }
+      }
+    }
+  }
+
+  .tip {
+    padding: 7px 16px;
+    background: #ebf1ff;
+    display: flex;
+    align-items: center;
+
+    > .icon {
+      font-size: 12px;
+      width: 12px;
+      height: 12px;
+      margin-right: 6px;
+      color: #1a58f4;
+    }
+
+    > span {
+      font-size: 12px;
+      color: var(--text-1);
+      line-height: 22px;
+    }
+  }
+
+  .operation {
+    margin-top: 24px;
+    text-align: center;
+  }
+</style>
