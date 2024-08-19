3b2b7659335f895fd9d92566c1d4ae236ec4f459
huangb <huangbing@ecdigit.com>
2024-08-19 13:37:42 +0800

    选择因子数据

--- src/views/carbon-report/components/FactorInput.vue
+++ src/views/carbon-report/components/FactorInput.vue
@@ -1,5 +1,67 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue';
+
+  const visible = ref(false);
+  const handleSwitch = () => {
+    visible.value = true;
+  };
+</script>
 
 <template>
-  <div> factor</div>
+  <div class="factor-input">
+    <iconpark-icon name="quesheng" class="icon" />
+    <a-input placeholder="请输入" class="input" />
+    <iconpark-icon name="switch" class="switch" @click="handleSwitch" />
+
+    <a-drawer
+      v-model:visible="visible"
+      title="选择因子数据"
+      :width="960"
+      :footer="false"
+    >
+      <div class="mb-24px">
+        <a-input
+          placeholder="请输入因子名称搜索"
+          class="w-300px mr-12px"
+        ></a-input>
+        <a-select placeholder="请选择行业" class="w-160px"></a-select>
+      </div>
+      <a-table></a-table>
+    </a-drawer>
+  </div>
 </template>
+
+<style lang="less" scoped>
+  .factor-input {
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    > .icon {
+      font-size: 10px;
+      width: 10px;
+      height: 10px;
+    }
+
+    > .input {
+      width: 110px;
+      margin: 0 4px;
+    }
+
+    > .switch {
+      font-size: 24px;
+      width: 24px;
+      height: 24px;
+      cursor: pointer;
+      display: none;
+      position: absolute;
+      right: 0;
+    }
+
+    &:hover {
+      > .switch {
+        display: inline-block;
+      }
+    }
+  }
+</style>
